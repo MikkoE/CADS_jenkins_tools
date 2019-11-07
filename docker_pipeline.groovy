e414c70b47950bdbe8c7fb74e94c8cad40327a7e
MikkoE <mikko.eberhardt@gmail.com>
2019-11-07 19:15:22 +0100

    changing pipeline syntax

--- docker_pipeline.groovy
+++ docker_pipeline.groovy
@@ -1,16 +1,13 @@
-pipeline {
-    agent {
-        node {
-          docker.withRegistry('cads-docker.cpt.haw-hamburg.de:5000'){
-            docker.image('cads-docker.cpt.haw-hamburg.de:5000/node')
-          }
-        }
-    }
-    stages {
-        stage('Test') {
-            steps {
-                sh 'node --version'
-            }
-        }
-    }
+#!groovy
+
+node {
+  docker.withRegistry('cads-docker.cpt.haw-hamburg.de:5000'){
+    docker.image('cads-docker.cpt.haw-hamburg.de:5000/node')
+  }
+
+  stage('Test') {
+      steps {
+          sh 'node --version'
+      }
+  }
 }
