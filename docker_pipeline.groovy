e19a423e186b98d60b2d2705b077e350a958ae58
Mikko <mikko.eberhardt@haw-hamburg.de>
2019-11-08 17:45:07 +0100

    adding inside docker

--- docker_pipeline.groovy
+++ docker_pipeline.groovy
@@ -3,11 +3,12 @@
 node {
   checkout scm
   docker.withRegistry('cads-docker.cpt.haw-hamburg.de:5000'){
-    docker.image('cads-docker.cpt.haw-hamburg.de:5000/node')
+    docker.image('cads-docker.cpt.haw-hamburg.de:5000/node').inside{
+      sh 'pwd'
+    }
   }
 
   stage('Test') {
     sh 'echo "Hello World!"'
-    sh 'node --version'
   }
 }
