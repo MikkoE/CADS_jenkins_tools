8b306c7131b64e3c868a2a137f1ca9f87ed4fae2
Mikko <mikko.eberhardt@haw-hamburg.de>
2019-11-09 12:55:22 +0100

    trying to set up inet workspace

--- pipeline.groovy
+++ pipeline.groovy
@@ -29,6 +29,7 @@ pipeline {
 
                         sh 'ls'
                         sh 'echo $PATH'
+                        sh 'whoami'
                         sh 'make'
                     }
                 }
