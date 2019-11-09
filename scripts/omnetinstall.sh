80072786b3f69b1b8cacf7df91a0aa69840a2441
Mikko <mikko.eberhardt@haw-hamburg.de>
2019-11-09 11:14:04 +0100

    deleting sudo in shell install

--- scripts/omnetinstall.sh
+++ scripts/omnetinstall.sh
@@ -1,10 +1,10 @@
 
-sudo apt-get update
-sudo apt-get install build-essential gcc g++ bison flex perl \
+apt-get update
+apt-get install build-essential gcc g++ bison flex perl \
  python python3 qt5-default libqt5opengl5-dev tcl-dev tk-dev \
  libxml2-dev zlib1g-dev default-jre doxygen graphviz libwebkitgtk-1.0
-sudo add-apt-repository ppa:ubuntugis/ppa
-sudo apt-get update
-sudo apt-get install openscenegraph-plugin-osgearth libosgearth-dev
-sudo apt-get install openmpi-bin libopenmpi-dev
-sudo apt-get install libpcap-dev
+add-apt-repository ppa:ubuntugis/ppa
+apt-get update
+apt-get install openscenegraph-plugin-osgearth libosgearth-dev
+apt-get install openmpi-bin libopenmpi-dev
+apt-get install libpcap-dev
