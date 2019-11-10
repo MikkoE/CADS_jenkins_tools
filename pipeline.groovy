3db6d91b03cd0ba9625b2161f15d946fa2e27513
Mikko <mikko.eberhardt@haw-hamburg.de>
2019-11-10 11:29:17 +0100

    trying to install inet

--- pipeline.groovy
+++ pipeline.groovy
@@ -31,6 +31,7 @@ pipeline {
                         sh 'echo $PATH'
                         sh 'cat README.md'
                         sh 'cat INSTALL'
+                        sh 'which nedtool'
                     }
                 }
             }
