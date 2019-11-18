3b51017deb2cc928b884b911f77ca211ca024ae1
Mikko <mikko.eberhardt@haw-hamburg.de>
2019-11-18 13:46:03 +0100

    command problem

--- pipeline.groovy
+++ pipeline.groovy
@@ -57,7 +57,7 @@ pipeline {
 
                         //trying to copy denis test for quic
                         sh 'cp quic-tests/QuicTLS.test inet-private/tests/unit/'
-                        sh 'cd inet-private/tests/unit/ && ll'
+                        sh 'cd inet-private/tests/unit/ && ls -l'
                         //sh 'cd inet-private/tests/packetdrill/quic/ && ./runtest'
                     }
                 }
