7fddad438a8634180e518dc3dd38f4c06f7c0a64
Mikko <mikko.eberhardt@haw-hamburg.de>
2019-12-17 13:26:58 +0100

    code cleanup

--- pipeline.groovy
+++ pipeline.groovy
@@ -8,7 +8,6 @@ pipeline {
     stages {
         stage('Preparation'){
             steps{
-                //sh 'ls'
                 sh 'id -u jenkins'
             }
         }
@@ -21,12 +20,6 @@ pipeline {
                     url: 'https://github.com/Transport-Protocol/inet-private.git'
                   sh 'git submodule update --init'
                 }
-                //dir('artifacts'){
-                //echo 'checkout artifact repo'
-                //git branch: 'master',
-                //  credentialsId: 'fd377909-72a2-44f5-b89e-787344533514',
-                //  url: 'https://github.com/MikkoE/test-results.git'
-                //}
             }
         }
         stage('docker-omnetpp'){
@@ -45,19 +38,15 @@ pipeline {
                         sh 'cd omnetpp && make -j$(grep -c proc /proc/cpuinfo)'
 
                         // installing inet
-                        //sh 'cd inet-private && cat README.md'
-                        //sh 'cd inet-private && cat INSTALL'
-                        //sh 'cd inet-private && cat Makefile'
                         sh 'cd inet-private && make makefiles'
                         sh 'cd inet-private && make MODE=debug'
 
-                        //running the tests
+                        //preparing for testing
                         sh 'cd inet-private/examples/quic/basic/ && rm simpleQuicSetup.ned'
                         sh 'cp quic-tests/testQuicSetup.ned inet-private/examples/quic/basic/'
 
                         //trying to copy test for quic
                         sh 'cp quic-tests/QuicHandshake.test inet-private/tests/unit/'
-                        //sh 'cd inet-private/tests/unit/ && ls -l'
 
                         //execute test
                         sh 'cd inet-private/tests/unit/ && ./runtest QuicHandshake.test'
